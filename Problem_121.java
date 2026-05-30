597d580372317e2b1702ee379b0dc4bfb02808a4
Jyoti Saini <136901655+jyotisaini10@users.noreply.github.com>
2026-05-30 21:51:56 +0530

    Add files via upload

---
+++ Problem_121.java
@@ -0,0 +1,28 @@
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a
+different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.*/
+
+class Problem_121 {
+     public static int maxProfit(int[] prices) {
+        int minPrice= Integer.MAX_VALUE;
+        int maxProfit=0;
+
+        for(int price: prices){
+            if(price<minPrice){
+                minPrice=price;
+            }
+            if(maxProfit< price-minPrice){
+                maxProfit= price-minPrice;
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[]= {12,1,4,5};
+        System.out.println(maxProfit(prices));
+    }
+
+}
