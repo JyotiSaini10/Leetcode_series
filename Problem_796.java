43931102091e47983909930bf12bd1ca8224b8f2
Jyoti Saini <136901655+JyotiSaini10@users.noreply.github.com>
2026-06-15 23:32:02 +0530

    Add files via upload

---
+++ Problem_796.java
@@ -0,0 +1,14 @@
+public class Problem_796 {
+        public static boolean rotateString(String s, String goal) {
+        if(s.length()!= goal.length()) return false;
+
+        return (s+s).contains(goal);
+    }
+
+    public static void main(String[] args) {
+        String s= "abcde";
+        String goal= "cdeab";
+
+        System.out.println(rotateString(s,goal));
+    }
+}
