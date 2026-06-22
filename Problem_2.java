70b35be6df2d112a9a75256dff9dccf0b5c61354
Jyoti Saini <136901655+JyotiSaini10@users.noreply.github.com>
2026-06-22 15:33:56 +0530

    Add files via upload

---
+++ Problem_2.java
@@ -0,0 +1,83 @@
+public class Problem_2 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry == 1) {
+
+            int sum = 0;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            sum += carry;
+            carry = sum / 10;
+
+            ListNode node = new ListNode(sum % 10);
+            temp.next = node;
+            temp = temp.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // l1 = 2 -> 4 -> 3
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        // l2 = 5 -> 6 -> 4
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        Problem_2 obj = new Problem_2();
+
+        ListNode result = obj.addTwoNumbers(l1, l2);
+
+        System.out.print("Result: ");
+        printList(result);
+    }
+}
